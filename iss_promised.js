2ba83514eba2df87e09eee9ffa464b7d2e20d273
Matija <matija_bozic@yahoo.com>
2021-12-03 06:53:31 +0000

    iss promised file

---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = body => {
+  const IP = JSON.parse(body);
+  return request(`https://freegeoip.app/json/${IP.ip}`);
+};
+
+const fetchISSFlyOverTimes = data => {
+  const coords = {};
+  coords['latitude'] = JSON.parse(data)['latitude'];
+  coords['longitude'] = JSON.parse(data)['longitude'];
+  return request(
+    `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`
+  );
+};
+
+const nextISSTimesForMyLocation = () => {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => {
+      const passTimes = JSON.parse(body).response;
+      return passTimes;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
