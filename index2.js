e3e534219ab05d120976ea516db040ac54cfb0ea
Matija <matija_bozic@yahoo.com>
2021-12-03 06:53:06 +0000

    index 2 file

---
+++ index2.js
@@ -0,0 +1,13 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then(body => {
+    for (const passTime of body) {
+      let date = new Date(passTime.risetime);
+      let duration = passTime.duration;
+      console.log(`Next pass ${date} for ${duration} seconds.`);
+    }
+  })
+  .catch(error => {
+    console.log("It didn't work: ", error.message);
+  });
